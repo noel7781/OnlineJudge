3c4b062ded15740cffd7939215ef0cfe300bdec8
SeungRyongHa <seungryong.ha@doublenc.com>
2022-02-28 17:56:53 +0900

    MenuBar 및 라우팅 구현

---
+++ web/src/components/TabBar.js
@@ -0,0 +1,11 @@
+// import useState from 'React'
+
+const TabBar = () => {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  )
+}
+
+export default TabBar
---
+++ web/src/pages/Problems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Problems</h1>
+      <p>문제 목록을 보여주는 페이지입니다.</p>
+    </div>
+  )
+}
+
+export default Home
